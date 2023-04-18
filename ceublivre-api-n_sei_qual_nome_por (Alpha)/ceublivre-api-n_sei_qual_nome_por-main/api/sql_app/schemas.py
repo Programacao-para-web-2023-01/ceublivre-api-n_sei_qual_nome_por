60368442286420d02f5fc2b58bd4e949b5023850
Danilocpm <79982991+Danilocpm@users.noreply.github.com>
2023-04-18 15:24:00 -0300

    Add files via upload

---
+++ ceublivre-api-n_sei_qual_nome_por (Alpha)/ceublivre-api-n_sei_qual_nome_por-main/api/sql_app/schemas.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from decimal import Decimal
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+# PEDIDO
+
+
+class PedidoBase(BaseModel):
+    valor_total: Optional[Decimal]
+    valor_frete: Optional[Decimal]
+    endereco_entrega: str
+
+
+class PedidoCreate(PedidoBase):
+    pass
+
+
+class PedidoUpdate(BaseModel):
+    endereco_entrega: str
+
+
+class Pedido(PedidoBase):
+    id: int
+    criado_em: datetime
+    produtos: List['Produto'] = []
+
+    class Config:
+        orm_mode = True
+
+
+# PRODUTO
+
+
+class ProdutoBase(BaseModel):
+    nome: str
+
+
+class ProdutoCreate(ProdutoBase):
+    pass
+
+
+class Produto(ProdutoBase):
+    id: int
+    pedidos: List[Pedido] = []
+
+    class Config:
+        orm_mode = True
+
+Pedido.update_forward_refs(Pedido=Pedido)
