6c99edbbd4098b7c39bbfc49cf0790013c863b5b
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-06-22 11:58:57 -0300

    implemented CORS

--- fastApiProject/main.py
+++ fastApiProject/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 from sqlalchemy.orm import Session
 from typing import List
@@ -8,8 +9,23 @@ from sql_app.database import SessionLocal, engine
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency for getting a database session
+
+
 def get_db():
     try:
         db = SessionLocal()
